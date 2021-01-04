743d6e15332c42ad02ded25beeb550e77fb98b11
Cyan Collier <cyan@incuna.com>
2021-01-04 19:19:46 +0000

    changed posts to use slugs intead of ids

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,9 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
  	list_display = ('title', 'author', 'created_date')
- 	fields = (('title', 'author'), 'text', 'created_date', 'published_date')
+ 	fields = (('title', 'author'), 'slug', 'text', 'created_date', 'published_date')
  	list_filter = ('title', 'author')
+ 	prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
 
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 
 
 class Post (models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=50, null=True, unique=True)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
@@ -16,3 +18,8 @@ class Post (models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def save(self, *args, **kwargs): 
+		if not self.slug:
+			self.slug = slugify(self.title)
+		return super().save(*args, **kwargs)
